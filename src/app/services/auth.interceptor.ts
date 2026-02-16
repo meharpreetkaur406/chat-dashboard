2edfd97da2b59898181f6486f1594efd59fb39f5
meharpreetkaur406 <meharpreetkaur406@example.com>
2026-02-16 11:55:40 +0530

    on session expiration navigate to login page

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -1,21 +1,39 @@
 // src/app/services/auth.interceptor.ts
 //An HTTP interceptor is a special Angular service that can intercept every HTTP request or response your app makes using HttpClient.
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
 import { UserLoginService } from './user-login.service';
+import { Router } from '@angular/router'
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private userLoginService: UserLoginService) {}
+  constructor(private userLoginService: UserLoginService, private router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const token = this.userLoginService.getToken();
+    let authReq = req;
     if (token) {
-      const cloned = req.clone({
-        headers: req.headers.set('Authorization', 'Bearer ' + token)
+      authReq = req.clone({
+       setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
       });
-      return next.handle(cloned);
     }
-    return next.handle(req);
+    return next.handle(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+
+        // TOKEN EXPIRED OR INVALID
+        if (error.status === 401) {
+            console.log("token got expired")
+          sessionStorage.removeItem('token');
+          alert('Session expired. Please login again.');
+          this.router.navigate(['/login']);
+        }
+
+        return throwError(() => error);
+      })
+    );
   }
 }
